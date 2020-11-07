f988076e554c312c19af5239634301e3a6149d26
zersh01 <zersh01@gmail.com>
2020-11-07 20:12:29 +0300

    First

---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageName = 'icq'
+$fileType = 'exe'
+$silentArgs = '-uninstall'
+$PATH = $Env:APPDATA
+$uninstallString = "$PATH/ICQ/bin/icqsetup.exe"
+
+if ($uninstallString -ne "") {
+    Uninstall-ChocolateyPackage $packageName $fileType $silentArgs $uninstallString
+}
+
