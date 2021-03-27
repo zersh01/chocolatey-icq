587c9579c88cd4b7d6dcccb25d8544310a6547d2
zersh01 <zersh01@gmail.com>
2021-03-27 19:45:51 +0300

    version 10.0.444115

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -9,7 +9,7 @@ $packageArgs = @{
   url           = $url
   softwareName  = 'icq' 
 
-  checksum      = 'E99D0A2328FB432991DEC2AC371A7C9B00383744F52FC97D5E7DE747E5BB3A15'
+  checksum      = '058BBC8F26A3DF31A1639D6EC517A57F949E1BCC3FF24C37541132EA436F8758'
   checksumType  = 'sha256'
 
   validExitCodes= @(0, 3010, 1641)
