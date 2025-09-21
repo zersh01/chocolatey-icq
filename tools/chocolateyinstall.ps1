85e6eb208c701ae80a547c91c39a06de1a90204e
zersh <zersh@mail.ru>
2025-09-21 20:15:45 +0300

    depricated

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,23 +1,7 @@
-﻿$ErrorActionPreference = 'Stop'; 
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://hb.bizmrg.com/icq-www/win/x32/icqsetup.exe' 
+﻿$ErrorActionPreference = 'Stop'
 
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE' 
-  url           = $url
-  softwareName  = 'icq' 
-
-  checksum      = '95412F7C8D00529A78BBC0EDDA899B05E24A1095A3D0550E49B6EA3A29A620AD'
-  checksumType  = 'sha256'
-
-  validExitCodes= @(0, 3010, 1641)
-  silentArgs   = '-appx' 
-}
-
-Install-ChocolateyPackage @packageArgs 
-
-
+Write-Warning "This package has been deprecated because the upstream project is discontinued."
+Write-Warning "No further updates will be provided."
+Write-Warning "Please uninstall this package by running: choco uninstall $env:ChocolateyPackageName -y"
 
 
